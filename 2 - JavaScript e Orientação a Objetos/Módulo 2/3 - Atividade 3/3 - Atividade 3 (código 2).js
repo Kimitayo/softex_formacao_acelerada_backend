d130f7836774b2aaa6f49adff6cfce463fa8ca32
Clara [Kimitayo] <84105466+Kimitayo@users.noreply.github.com>
2023-09-14 20:56:57 -0300

    Create 3 - Atividade 3 (código 2).js

---
+++ 2 - JavaScript e Orientação a Objetos/Módulo 2/3 - Atividade 3/3 - Atividade 3 (código 2).js
@@ -0,0 +1,11 @@
+let nota1 = parseFloat(prompt("Digite a primeira nota: "));
+let nota2 = parseFloat(prompt("Digite a segunda nota: "));
+
+
+let mediaAtual = (nota1 + nota2) / 2;
+
+
+let notaMinima = 7 * 2 - (nota1 + nota2);
+
+
+console.log(`Nota mínima: ${notaMinima}`);
